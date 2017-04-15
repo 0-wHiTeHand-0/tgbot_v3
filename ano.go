35c92d3d250542c5316257aed8384b9d8e2bf398
root <root@localhost.localdomain>
2017-04-15 07:32:56 +0200

    Added pagination in ANO search and send pictures size

--- ano.go
+++ ano.go
@@ -12,6 +12,7 @@ import (
     "net/http"
     "regexp"
     "strings"
+    "strconv"
 )
 
 const picsURL = "http://ano.lolcathost.org/pics/"
@@ -25,11 +26,13 @@ func NewCmdAno(config *CmdConfigAno) {
     config.Reg = regexp.MustCompile(`^(?:.+$)`)
 }
 
-func AnoRunRandom() ([]string,error) {
+func AnoRunRandom() ([]string, []int, []int, error) {
 
     var respData struct {
         Pics []struct {
-            ID string
+            ID string `json:"id"`
+            Sizew int `json:"sizew"`
+            Sizeh int `json:"sizeh"`
         }
     }
     reqData := struct {
@@ -37,80 +40,104 @@ func AnoRunRandom() ([]string,error) {
         Num     int     `json:"num"`
     }{
         "random",
-        15,
+        25,
     }
     reqBody, err := json.Marshal(reqData)
     if err != nil {
-        return []string{}, err
+        return []string{}, []int{},[]int{}, err
     }
     resp, err := http.Post("http://ano.lolcathost.org/json/pic.json",
     "application/json", bytes.NewReader(reqBody))
     if err != nil {
-        return []string{}, err
+        return []string{}, []int{}, []int{}, err
     }
     defer resp.Body.Close()
 
     if resp.StatusCode != http.StatusOK {
-        return []string{}, fmt.Errorf("HTTP error: %v (%v)", resp.Status, resp.StatusCode)
+        return []string{}, []int{}, []int{}, fmt.Errorf("HTTP error: %v (%v)", resp.Status, resp.StatusCode)
     }
     repBody, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return []string{}, err
+        return []string{}, []int{}, []int{}, err
     }
     err = json.Unmarshal(repBody, &respData)
     if err != nil {
-        return []string{}, err
+        return []string{}, []int{}, []int{}, err
     }
     var IDs []string
+    var width,height []int
     for _, tmp := range respData.Pics{
         IDs = append(IDs, picsURL + tmp.ID)
+        height = append(height, tmp.Sizeh)
+        width = append(width, tmp.Sizew)
     }
-    return IDs, nil
+    return IDs, height, width, nil
 }
 
-func AnoRunTags(in string) ([]string, error){
+func AnoRunTags(in string, offset string) ([]string, string, []int, []int, error){
 
     var respData struct {
         Pics []struct {
-            ID string
+            ID string `json:"id"`
+            Sizew int `json:"sizew"`
+            Sizeh int `json:"sizeh"`
         }
+        Total   string  `json:"total"`
     }
     var IDs []string
+    var newoffset string
 
     reqData := struct {
         Method string   `json:"method"`
+        Offset int      `json:"offset"`
         Tags   []string `json:"tags"`
         Limit  int      `json:"limit"`
     }{
         "searchRelated",
+        -1,
         strings.SplitN(in, ",", -1),
         25,
     }
 
+    ioffset, err := strconv.Atoi(offset)
+    if (ioffset < 0) || (err != nil){
+        reqData.Offset = 0
+    }else{
+        reqData.Offset = ioffset
+    }
     reqBody, err := json.Marshal(reqData)
     if err != nil {
-        return []string{}, err
+        return []string{}, "", []int{}, []int{}, err
     }
     resp, err := http.Post("http://ano.lolcathost.org/json/tag.json",
     "application/json", bytes.NewReader(reqBody))
     if err != nil {
-        return []string{}, err
+        return []string{}, "", []int{}, []int{}, err
     }
     defer resp.Body.Close()
 
     if resp.StatusCode != http.StatusOK {
-        return []string{}, fmt.Errorf("HTTP error: %v (%v)", resp.Status, resp.StatusCode)
+        return []string{}, "", []int{}, []int{}, fmt.Errorf("HTTP error: %v (%v)", resp.Status, resp.StatusCode)
     }
     respBody, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        return []string{}, err
+        return []string{}, "", []int{}, []int{}, err
     }
     err = json.Unmarshal(respBody, &respData)
     if err != nil {
-        return []string{}, err
+        return []string{}, "", []int{}, []int{}, err
     }
+
+    var width,height []int
     for _, tmp := range respData.Pics{
         IDs = append(IDs, picsURL + tmp.ID)
+        height = append(height, tmp.Sizeh)
+        width = append(width, tmp.Sizew)
     }
-    return IDs, nil
+    totalint , err := strconv.Atoi(respData.Total)
+    newoffset = ""
+    if ioffset + reqData.Limit < totalint{
+        newoffset = strconv.Itoa(ioffset + reqData.Limit)
+    }
+    return IDs, newoffset, height, width, err
 }
--- main.go
+++ main.go
@@ -7,11 +7,10 @@ import (
     "io/ioutil"
     "encoding/json"
     "github.com/go-telegram-bot-api/telegram-bot-api"
-		"github.com/robfig/cron"
-		"time"
+    "github.com/robfig/cron"
+    "time"
     "math/rand"
-   	"strings"
-	 	"strconv"
+    "strings"
     "crypto/md5"
     "encoding/hex"
 )
@@ -41,8 +40,8 @@ type cmdConfigs struct {
     Ano     CmdConfigAno        `json:"ano"`
     Chive   CmdConfigChive      `json:"chive"`
     Voice   CmdConfigVoice      `json:"voice"`
-		Fcdg		CmdConfigFcdg				`json:"4cdg"`
-		Ctftime	CmdConfigCtftime		`json:"ctftime"`
+    Fcdg		CmdConfigFcdg				`json:"4cdg"`
+    Ctftime	CmdConfigCtftime		`json:"ctftime"`
 }
 
 func send_pic(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, path string, f bool){
@@ -96,137 +95,145 @@ func main() {
     u := tgbotapi.NewUpdate(0)
     u.Timeout = 60
 
-		if Commandssl.Ctftime.Enabled{
-						c := cron.New()
-						c.AddFunc("0 0 18 * * 3", func() {//Every thursday at 18.00
-										err, txt := Ctftime_apireq()
-										if err != nil{
-														log.Println(err)
-														return
-										}
-										for _, i := range Commandssl.Ctftime.Channel_ids{
-														msg := tgbotapi.NewMessage(int64(i), txt)
-														msg.ParseMode = "Markdown"
-														bot.Send(msg)
-										}
-										log.Println("Sent CTFtime upcoming events!")
-						})
-						c.Start()
-		}
+    if Commandssl.Ctftime.Enabled{
+        c := cron.New()
+        c.AddFunc("0 0 18 * * 3", func() {//Every thursday at 18.00
+            err, txt := Ctftime_apireq()
+            if err != nil{
+                log.Println(err)
+                return
+            }
+            for _, i := range Commandssl.Ctftime.Channel_ids{
+                msg := tgbotapi.NewMessage(int64(i), txt)
+                msg.ParseMode = "Markdown"
+                bot.Send(msg)
+            }
+            log.Println("Sent CTFtime upcoming events!")
+        })
+        c.Start()
+    }
 
-		updates, err := bot.GetUpdatesChan(u)
-		for update := range updates {
-						if (update.Message!=nil){
-										if banned_user(update.Message.From){
-														log.Println("Banned user " + update.Message.From.FirstName + " blocked!")
-														continue
-										}
-										log.Printf("Message -> [%s] %s (id: %s)", update.Message.From.FirstName, update.Message.Text, strconv.FormatInt(update.Message.Chat.ID, 10))
-										var msg tgbotapi.MessageConfig
-										if (Commandssl.Ban.Enabled && Commandssl.Ban.Reg.MatchString(update.Message.Text)){
-														txt := BanRun(update.Message)
-														if (txt!=""){
-																		msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
-																		msg.ReplyToMessageID = update.Message.MessageID
-														}else{
-																		send_pic(bot, update.Message, "a12.jpg" , true)
-																		continue
-														}
-										}else if (Commandssl.Quotes.Enabled && Commandssl.Quotes.Reg.MatchString(update.Message.Text)){
-														txt := QuotesRun(update.Message)
-														if (txt!=""){
-																		msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
-																		msg.ParseMode = "Markdown"
-														}else{
-																		send_pic(bot, update.Message, "a12.jpg", true)
-																		continue
-														}
-										}else if (Commandssl.Chive.Enabled && Commandssl.Chive.Reg.MatchString(update.Message.Text)){
-														msg = tgbotapi.NewMessage(update.Message.Chat.ID, ChiveRun(update. Message.Text))
-										}else if (Commandssl.Voice.Enabled && Commandssl.Voice.Reg.MatchString(update.Message.Text)){
-														b, st := VoiceRun(update.Message.Text)
-														if st == ""{
-																		bot.Send(tgbotapi.NewVoiceUpload(update.Message.Chat.ID, bytes_to_filebytes(b)))
-																		continue
-														}else{
-																		msg = tgbotapi.NewMessage(update.Message.Chat.ID, st)
-														}
-										}else if (Commandssl.Fcdg.Enabled && Commandssl.Fcdg.Reg.MatchString(update.Message.Text)){
-														txt, err := FcdgRun(update.Message.Text)
-														if err != nil{
-																		log.Fatalln(err)
-														}
-														if (update.Message.Text == "/4cdg rules") {
-																		msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
-																		msg.ParseMode = "Markdown"
-														}else{
-																		send_pic(bot, update.Message, txt, false)
-																		continue
-														}
-										}else if((update.Message.Text=="")||(update.Message.ReplyToMessage!=nil)){
-														continue
-										}else{
-														m := "Command not found!"
-														if (Commandssl.Bitchx.Enabled){m = BitchxRun(update.Message.From.FirstName)}
-														msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
-														msg.ReplyToMessageID = update.Message.MessageID
-										}
-										bot.Send(msg)
-						}else if (update.InlineQuery!=nil){
-										if banned_user(update.InlineQuery.From){
-														log.Println("Banned user " + update.InlineQuery.From.FirstName + " blocked!")
-														continue
-										}
-										log.Printf("Inline -> [%s] %s (alias: %s)", update.InlineQuery.From.FirstName, update.InlineQuery.Query, update.InlineQuery.From.UserName)
-										var resu []interface{}
-										var ano_urls []string
-										var err error
-										random := false
-										if update.InlineQuery.Query == ""{
-														ano_urls, err = AnoRunRandom()
-														if err != nil{
-																		log.Println(err)
-																		continue
-														}
-														random = true
-										}else if Commandssl.Ano.Reg.MatchString(update.InlineQuery.Query){
-														ano_urls, err = AnoRunTags(update.InlineQuery.Query)
-														if err != nil{
-																		log.Println(err)
-																		continue
-														}
-										}
-										for _, ano_url := range ano_urls{
-														hasher := md5.New()
-														hasher.Write([]byte(ano_url))
-														tmpid := hex.EncodeToString(hasher.Sum(nil))
-														if (strings.Contains(ano_url, ".jpg")||strings.Contains(ano_url, ".png")||strings.Contains(ano_url, ".jpeg")){
-																		tmp1:=tgbotapi.NewInlineQueryResultPhoto(tmpid, ano_url)
-																		tmp1.ThumbURL = ano_url
-																		resu = append(resu, tmp1)
-														}else if strings.Contains(ano_url, ".gif"){
-																		tmp1:=tgbotapi.NewInlineQueryResultGIF(tmpid, ano_url)
-																		tmp1.ThumbURL = ano_url
-																		resu = append(resu, tmp1)
-														}else{
-																		log.Println("ANO is returning strange replies: " + ano_url)
-														}
-										}
-										if len(ano_urls)==0{
-														log.Println("Bad syntax, or No pics found!")
-										}
-										inline := tgbotapi.InlineConfig{
-														InlineQueryID: update.InlineQuery.ID,
-														IsPersonal: false,
-														CacheTime: 0,
-														Results: resu,
-										}
-										if random{
-														inline.NextOffset = "1"
-										}
-										if _, err := bot.AnswerInlineQuery(inline); err != nil {
-														log.Println(err)
-										}
-						}
-		}
+    updates, err := bot.GetUpdatesChan(u)
+    for update := range updates {
+        if (update.Message!=nil){
+            if banned_user(update.Message.From){
+                log.Println("Banned user " + update.Message.From.FirstName + " blocked!")
+                continue
+            }
+            log.Printf("Message -> [%s] %s (id: %d, alias: %s)", update.Message.From.FirstName, update.Message.Text, update.Message.Chat.ID, update.Message.From.UserName)
+            var msg tgbotapi.MessageConfig
+            if (Commandssl.Ban.Enabled && Commandssl.Ban.Reg.MatchString(update.Message.Text)){
+                txt := BanRun(update.Message)
+                if (txt!=""){
+                    msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
+                    msg.ReplyToMessageID = update.Message.MessageID
+                }else{
+                    send_pic(bot, update.Message, "a12.jpg" , true)
+                    continue
+                }
+            }else if (Commandssl.Quotes.Enabled && Commandssl.Quotes.Reg.MatchString(update.Message.Text)){
+                txt := QuotesRun(update.Message)
+                if (txt!=""){
+                    msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
+                    msg.ParseMode = "Markdown"
+                }else{
+                    send_pic(bot, update.Message, "a12.jpg", true)
+                    continue
+                }
+            }else if (Commandssl.Chive.Enabled && Commandssl.Chive.Reg.MatchString(update.Message.Text)){
+                msg = tgbotapi.NewMessage(update.Message.Chat.ID, ChiveRun(update. Message.Text))
+            }else if (Commandssl.Voice.Enabled && Commandssl.Voice.Reg.MatchString(update.Message.Text)){
+                b, st := VoiceRun(update.Message.Text)
+                if st == ""{
+                    bot.Send(tgbotapi.NewVoiceUpload(update.Message.Chat.ID, bytes_to_filebytes(b)))
+                    continue
+                }else{
+                    msg = tgbotapi.NewMessage(update.Message.Chat.ID, st)
+                }
+            }else if (Commandssl.Fcdg.Enabled && Commandssl.Fcdg.Reg.MatchString(update.Message.Text)){
+                txt, err := FcdgRun(update.Message.Text)
+                if err != nil{
+                    log.Fatalln(err)
+                }
+                if (update.Message.Text == "/4cdg rules") {
+                    msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
+                    msg.ParseMode = "Markdown"
+                }else{
+                    send_pic(bot, update.Message, txt, false)
+                    continue
+                }
+            }else if((update.Message.Text=="")||(update.Message.ReplyToMessage!=nil)){
+                continue
+            }else{
+                m := "Command not found!"
+                if (Commandssl.Bitchx.Enabled){m = BitchxRun(update.Message.From.FirstName)}
+                msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
+                msg.ReplyToMessageID = update.Message.MessageID
+            }
+            bot.Send(msg)
+        }else if (update.InlineQuery!=nil){
+            if banned_user(update.InlineQuery.From){
+                log.Println("Banned user " + update.InlineQuery.From.FirstName + " blocked!")
+                continue
+            }
+            log.Printf("Inline -> [%s] %s (alias: %s)", update.InlineQuery.From.FirstName, update.InlineQuery.Query, update.InlineQuery.From.UserName)
+            var resu []interface{}
+            var ano_urls []string
+            var height,width []int
+            var offset, newoffset string
+            var err error
+            if update.InlineQuery.Query == ""{
+                newoffset = "1"
+                ano_urls, height, width, err = AnoRunRandom()
+                if err != nil{
+                    log.Println(err)
+                    continue
+                }
+            }else if Commandssl.Ano.Reg.MatchString(update.InlineQuery.Query){
+                if update.InlineQuery.Offset == ""{
+                    offset = "0"
+                }else{
+                    offset = update.InlineQuery.Offset
+                }
+                ano_urls, newoffset, height, width, err = AnoRunTags(update.InlineQuery.Query, offset)
+                if err != nil{
+                    log.Println(err)
+                    continue
+                }
+            }
+            for i, ano_url := range ano_urls{
+                hasher := md5.New()
+                hasher.Write([]byte(ano_url))
+                tmpid := hex.EncodeToString(hasher.Sum(nil))
+                if (strings.Contains(ano_url, ".jpg")||strings.Contains(ano_url, ".png")||strings.Contains(ano_url, ".jpeg")){
+                    tmp1:=tgbotapi.NewInlineQueryResultPhoto(tmpid, ano_url)
+                    tmp1.ThumbURL = ano_url
+                    tmp1.Height = height[i]
+                    tmp1.Width = width[i]
+                    resu = append(resu, tmp1)
+                }else if strings.Contains(ano_url, ".gif"){
+                    tmp1:=tgbotapi.NewInlineQueryResultGIF(tmpid, ano_url)
+                    tmp1.ThumbURL = ano_url
+                    tmp1.Height = height[i]
+                    tmp1.Width = width[i]
+                    resu = append(resu, tmp1)
+                }else{
+                    log.Println("ANO is returning strange replies: " + ano_url)
+                }
+            }
+            if len(ano_urls)==0{
+                log.Println("Bad syntax, or No pics found!")
+            }
+            inline := tgbotapi.InlineConfig{
+                InlineQueryID: update.InlineQuery.ID,
+                IsPersonal: false,
+                CacheTime: 0,
+                Results: resu,
+                NextOffset: newoffset,
+            }
+            if _, err := bot.AnswerInlineQuery(inline); err != nil {
+                log.Println(err)
+            }
+        }
+    }
 }
